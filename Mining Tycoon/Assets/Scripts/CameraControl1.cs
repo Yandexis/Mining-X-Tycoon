3b7e1d8c92187c92dbb1be6861375c0fc4a35c08
Vasya <vasyakov4uk@gmail.com>
2021-02-18 16:55:01 +0200

    Project v0.01

---
+++ Mining Tycoon/Assets/Scripts/CameraControl1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl1 : MonoBehaviour
+{
+    public float Speed;
+    public float MinCordX;
+    public float MaxCordX;    
+
+    private Vector2 startingPosition;
+    private Camera camera;
+    private float targetPosition;
+
+    private void Start()
+    {
+        camera = GetComponent<Camera>();
+        targetPosition = transform.position.x;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) startingPosition = camera.ScreenToWorldPoint(Input.mousePosition);
+        else if (Input.GetMouseButton(0))
+        {
+            float position = (camera.ScreenToWorldPoint(Input.mousePosition).x - startingPosition.x) * Speed;
+            targetPosition = Mathf.Clamp(transform.position.x - position, MinCordX, MaxCordX);
+        }
+        transform.position = new Vector3(Mathf.Lerp(transform.position.x, targetPosition, (Speed + 3) * Time.deltaTime), transform.position.y, transform.position.z);
+    }
+}
